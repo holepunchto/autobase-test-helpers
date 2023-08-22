53880b4d2bb446fb78d03580ac5308e7f9fb662b
Mathias Buus <mathiasbuus@gmail.com>
2023-08-22 12:59:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# autobase-test-helpers
+
+Helpers when writing tests for an Autobased application
+
+```
+npm install autobase-test-helpers
+```
+
+## Usage
+
+``` js
+const { downloadAll } = require('autobase-test-helpers')
+
+await downloadAll(bases) // expects replication to have been set up
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = { downloadAll, eventFlush }
+
+function eventFlush () {
+  return new Promise(resolve => setImmediate(resolve))
+}
+
+async function downloadAll (bases, flush = eventFlush) {
+  do {
+    await flush()
+    await Promise.all(bases.map(downloadAllWriters, flush))
+    await flush()
+  } while (!synced(bases))
+}
+
+function synced (bases) {
+  for (let i = 0; i < bases.length; i++) {
+    for (const w of bases[i].writers) {
+      if (w.core.length !== w.core.contiguousLength) return false
+    }
+  }
+  return true
+}
+
+async function downloadAllWriters (base, flush = eventFlush) {
+  await base.ready()
+  let writers = 0
+
+  do {
+    writers = base.writers.length
+    for (const w of base.writers) {
+      await flush()
+      await w.core.update({ wait: true })
+      await coreDownloadAll(w.core)
+    }
+    await base.update()
+  } while (writers !== base.writers.length)
+}
+
+function coreDownloadAll (core) {
+  const start = core.contiguousLength
+  const end = core.length
+  return core.download({ start, end }).done()
+}
